b48cdfd39f6a76ffcf18530d5f52fc8c0122c804
hyunsik4476 <hyunsik4476@gmail.com>
2022-01-19 18:22:59 +0900

    람다/ 필터 함수 관련 정리

--- 0119/0119_p_6.py
+++ 0119/0119_p_6.py
@@ -3,7 +3,13 @@ def odd(n):
     return n % 2 # ???
 
 print(list(filter(odd, range(5))))
+# 이게 왜 작동하나?
+# 첫 추측: return 1 = n, return 0 =None ???
+# print(odd(n)) 으로 확인해봄 -> 아님
+# 해결: filter 함수에서 function 은 T/ F 판단
 
 print(list(filter(lambda n: n % 2, range(5))))
 
 print(list(filter(lambda n: n  if n % 2 == 1 else None, range(5))))
+# 실상 이럴 필요가 없었음: n % 2 == 1 자체가 T/ F 를 반환하므로
+# n 을 리턴하는 것에 대해: filter 함수의 동작에 대한 이해 부족
